4e33c03aeb5c4cc186ecaaafe8cbd94c17bec7ef
blooodoopy <blooo_doopy@hotmail.com>
2018-05-21 10:35:14 -0300

    agora foi, initial files

---
+++ src/javascript/app.js
@@ -0,0 +1,14 @@
+//make sute to import vue on the actual environment later
+//add webpack...
+new Vue({
+  el: ".container",
+  data: {
+    
+  },
+  computed: {
+    
+  },
+  methods: {
+    
+  }
+})
